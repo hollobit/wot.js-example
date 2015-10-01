4d5f1901ff5f814bab3597ca9a3b784962e50da0
JoonHo Son <jhson@nagesoft.com>
2015-10-02 00:31:46 +0900

    * Sensor/Acturator 추가.
    - Motion sensor 추가.
    - LED => Motion sensor에 따른 on/off(테스트용)

--- coex_rb_room1/app.js
+++ coex_rb_room1/app.js
@@ -4,8 +4,8 @@ var express = require('express');
 var http = require('http');
 var express = require('express');
 var parser = require('body-parser');
+var wot = require('../etri/wot-enhanced/index');
 // var wot = require('wotjs');
-var wot = require('wotjs');
 var async = require('async');
 var _ = require('lodash');
 var log4js = require('log4js');
@@ -76,9 +76,56 @@ async.series([
     function (done) {
         done();
 
+        var client = require('socket.io-client')('http://localhost:8088', {
+            reconnection: true,
+            reconnectionDelay: 500
+        });
+
+        // client.connect(port, '10.0.1.5', function () {
+        //     log.debug('*********************************');
+        //     log.debug('Socket connected.');
+        //     log.debug('*********************************');
+        // });
+
+        client.on('sensorData', function (data) {
+            log.debug(data);
+
+            if (data.type === 'motion') {
+                log.info('motion value >>> ', data.value);
+
+                var commands = [{pin: 18, command: (data.value === 1)? 'on': 'off'}];
+
+                wot.setActurators('gpio', 'rgbLed', commands, null);
+            }
+        });
+
+        client.on('disconnect', function () {
+            log.info('*********************************');
+            log.info('Socket closed.');
+            log.info('*********************************');
+        });
+
+        client.on('reconnecting', function () {
+            log.info('Try reconnecting socket...');
+        });
+
+        client.on('reconnect', function () {
+            log.info('Reconnect success!');
+        });
+
+        client.on('reconnect_error', function() {
+            log.fatal('Reconnect socket failed.\nExit.');
+            process.exit(1);
+        });
+    },
+    function (done) {
+        done();
+
         var ds18b20 = 'sensorjs:///w1/28-000005559410/ds18b20/28-000005559410';
         var bh1750fvi = 'sensorjs:///i2c/0x23/BH1750/BH1750-0x23';
         var htu21d = 'sensorjs:///i2c/0x40/HTU21D/HTU21D-0x40';
+        var led = 'sensorjs:///gpio/18/rgbLed/rgbLed-18';
+        var motion = 'sensorjs:///gpio/24/motionDetector/motion-24';
 
         wot.createSensor(ds18b20, function (error, data) {
             if (error) {
@@ -107,6 +154,15 @@ async.series([
         wot.createSensor(htu21d, function (error, data) {
             if (error) {
                 log.error('HTU21D registration fail!! -> ', error);
+                process.exit(1);
+            } else {
+
+            }
+        });
+
+        wot.createSensor(motion, function (error, data) {
+            if (error) {
+                log.error('Motion registration fail!!! -> ', error);
                 process.exit(1);
             } else {
 
